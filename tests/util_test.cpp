d19e463eacf0ee7fa3459ff88cd6d19ae04890e5
Jens Edhammer <tobyndax@gmail.com>
2021-12-03 17:23:01 +0100

    raw text line parsing

--- tests/util_test.cpp
+++ tests/util_test.cpp
@@ -17,3 +17,14 @@ TEST(UtilTest, CanParseSimpleIntegers) {
     EXPECT_EQ(ints[i], gt[i]) << "Vectors ints and gt differ at index " << i;
   }
 }
+
+TEST(UtilTest, CanParseBinaryGrid) {
+  std::string path = "tests/day3/testdata.txt";
+  FileReader fileReader(path);
+
+  std::vector<std::string> data = fileReader.getAsStrings();
+
+  ASSERT_EQ(data[0], "00100");
+  ASSERT_EQ(data[4], "10101");
+  ASSERT_EQ(12, data.size());
+}
