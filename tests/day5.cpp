6e000a06a1ebf8c03639a3fcd783b01dd5173bad
Jens Edhammer <tobyndax@gmail.com>
2021-12-05 17:10:47 +0100

    Part1 Day5

--- tests/day5.cpp
+++ tests/day5.cpp
@@ -2,7 +2,19 @@
 #include "src/lib/util.h"
 #include "gtest/gtest.h"
 
-TEST(Day5Test, CanConstructGrid) {
+TEST(Day5Test, CalcDangerSum) {
+
+  std::string path = "tests/day5/testdata.txt";
+  FileReader fileReader(path);
+
+  std::vector<std::pair<Point3D, Point3D>> data =
+      fileReader.getAsLineCoordinates();
+  Day5Solver solver(data);
+  int dangerSum = solver.solve(true);
+  ASSERT_EQ(dangerSum, 5);
+}
+
+TEST(Day5Test, CalcDangerSumWithDiagonals) {
 
   std::string path = "tests/day5/testdata.txt";
   FileReader fileReader(path);
