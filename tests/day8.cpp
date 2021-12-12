9e2095ef170ecf96b957aff163c4f95c1620cf92
Jens Edhammer <tobyndax@gmail.com>
2021-12-12 15:51:47 +0100

    Fix test name duplication

--- tests/day8.cpp
+++ tests/day8.cpp
@@ -2,7 +2,7 @@
 #include "src/lib/util.h"
 #include "gtest/gtest.h"
 
-TEST(Day8Test, Part1) {
+TEST(Day8Test, ParseInput) {
   FileReader fileReader("tests/day8/testdata.txt");
   ASSERT_EQ(false, true);
 }
